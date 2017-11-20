62fbae5c32cd4cf980582ecccb6d12980712324d
jiun\jiunoh <juneoh227@gmail.com>
2017-11-20 21:44:24 +0900

    메모 추가 완료

--- src/MemoManager.java
+++ src/MemoManager.java
@@ -1,15 +1,28 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MemoManager extends Menu {
+	private ArrayList<String> memoList = new ArrayList<String>();
+	private static final String FILENAME = "memoManager.txt";
+	private boolean isModified=true;
 	
 	public void showAndSelect() {
 		Scanner scan = new Scanner(System.in);
-		int n = scan.nextInt();
+		readMemoFile();
+		int n=0;
+		
 		do {
 			printMenu();
+			System.out.print("입력: ");
+			n = scan.nextInt();			
+			
 			switch (n) {
 			case 1:
-				createMemo();
+				addMemo();
 				break;
 			case 2:
 				listMemo();
@@ -22,10 +35,48 @@ public class MemoManager extends Menu {
 				break;
 			}
 		} while (n != 5);
+		
+		if (isModified)
+			writeMemoFile();
   }
 	
-	private void createMemo() {
-		
+	private void readMemoFile() {
+		try {
+			initializeList();
+		}
+		catch (IOException e) {
+			;
+		}
+	}
+	
+	private void initializeList() throws IOException {
+		while (true) {
+			BufferedReader bufferedReader = new BufferedReader(new FileReader(FILENAME));
+			String line = bufferedReader.readLine();
+			if (line == null)
+				break;
+			memoList.add(line);
+		}
+	}
+	
+	private void addMemo() {
+		Scanner scan = new Scanner(System.in);		
+		System.out.print("메모 입력: ");
+		String memo = scan.nextLine();
+		memoList.add(memo);		
+		isModified = true;
+	}
+	
+	private void writeMemoFile() {
+        try {
+        	PrintWriter printWriter = new PrintWriter(FILENAME);
+        	for (int i=0; i<memoList.size(); i++)
+        		printWriter.println(memoList.get(i));
+            printWriter.close();
+            
+        } catch (Exception e) {
+        	System.out.println("파일을 찾을 수 없습니다");
+        }
 	}
 	
 	private void listMemo() {
