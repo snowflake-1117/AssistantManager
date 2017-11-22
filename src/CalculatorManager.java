c8a8303a1159c43013c8b56237a631de68eee01d
Hyemin Kim <1117hyemin@gmail.com>
2017-11-22 17:39:24 +0900

    계산기 매니저 showAndSelect 함수 작성 완료
    각 경우에 대한 케이스 작성, 제시되지 않은 숫자 범위 입력 시 그에 대한 알림 출력

--- src/CalculatorManager.java
+++ src/CalculatorManager.java
@@ -1,12 +1,41 @@
+import java.util.Scanner;
 
 public class CalculatorManager extends Menu {
     @Override
     public void showAndSelect() {
+        int choice = -1;
+        Scanner scanner = new Scanner(System.in);
 
+        while (choice != 4) {
+            printMenu();
+            choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    calculate();
+                    break;
+                case 2:
+                    UnitConversionManager unitConversionManager = new UnitConversionManager();
+                    unitConversionManager.showAndSelect();
+                    break;
+                case 3:
+                    break;
+                default:
+                    String wrongInput = "잘못된 입력 값 입니다.";
+                    System.out.println(wrongInput);
+                    break;
+            }
+        }
+    }
+
+    private void calculate() {
     }
 
     @Override
     protected void printMenu() {
-
+        System.out.println("1. 계산기");
+        System.out.println("2. 단위변환");
+        System.out.println("3. 뒤로가기");
+        System.out.print("입력: ");
     }
 }
