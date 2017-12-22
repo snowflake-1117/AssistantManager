85ff8edd77b135d118717b78db1fa8063b8ea19c
Hyemin Kim <1117hyemin@gmail.com>
2017-12-22 10:58:52 +0900

    수식 계산 테스트 코드 추가

--- src/CalculatorManagerTest.java
+++ src/CalculatorManagerTest.java
@@ -1,5 +1,16 @@
+import org.junit.*;
 import static org.junit.Assert.*;
 
 public class CalculatorManagerTest {
-
+    @Test
+    public void calculatorTest() {
+        CalculatorManager calculatorManager = new CalculatorManager();
+        assertEquals("5.5", calculatorManager.calculate("2+3.5"));
+        assertEquals("-1.5", calculatorManager.calculate("2-3.5"));
+        assertEquals("7", calculatorManager.calculate("2*3.5"));
+        assertEquals("-7", calculatorManager.calculate("2*-3.5"));
+        assertEquals("3.5", calculatorManager.calculate("7/2"));
+        assertEquals("-3.5", calculatorManager.calculate("7/-2"));
+        assertEquals("89", calculatorManager.calculate("(2-3*6)/2+4/(-2)+9.9*10"));
+    }
 }
