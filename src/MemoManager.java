16f2150071014e20e45430ae299c2f89eb9789f7
jiun\jiunoh <juneoh227@gmail.com>
2017-12-23 15:08:43 +0900

    리팩토링(인덴트 수정, 순서 변경, 디폴트 메뉴 추가)

--- src/MemoManager.java
+++ src/MemoManager.java
@@ -9,17 +9,17 @@ public class MemoManager extends Menu {
 	protected ArrayList<String> memoList = new ArrayList<String>();
 	private static final String FILENAME = "memoManager.txt";
 	protected boolean isModified = false;
-	
+
 	public void showAndSelect() {
 		Scanner scan = new Scanner(System.in);
 		readMemoFile();
-		int n=0;
-		
+		int n = 0;
+
 		do {
 			printMenu();
 			System.out.print("입력: ");
-			n = scan.nextInt();			
-			
+			n = scan.nextInt();
+
 			switch (n) {
 			case 1:
 				addMemo();
@@ -33,24 +33,25 @@ public class MemoManager extends Menu {
 			case 4:
 				deleteMemo();
 				break;
+			default:
+				System.out.println("잘못된 입력입니다.");
 			}
 		} while (n != 5);
-		
+
 		if (isModified)
 			writeMemoFile();
-  }
-	
+	}
+
 	private void readMemoFile() {
 		try {
 			initializeList();
-		}
-		catch (IOException e) {
+		} catch (IOException e) {
 			return;
 		}
 	}
-	
+
 	private void initializeList() throws IOException {
-		BufferedReader bufferedReader = new BufferedReader(new FileReader(FILENAME));		
+		BufferedReader bufferedReader = new BufferedReader(new FileReader(FILENAME));
 		while (true) {
 			String line = bufferedReader.readLine();
 			if (line == null)
@@ -59,67 +60,67 @@ public class MemoManager extends Menu {
 		}
 		bufferedReader.close();
 	}
-	
+
+	protected void writeMemoFile() {
+		try {
+			PrintWriter printWriter = new PrintWriter(FILENAME);
+			for (int i = 0; i < memoList.size(); i++)
+				printWriter.println(memoList.get(i));
+			printWriter.close();
+		} catch (Exception e) {
+			System.out.println("파일을 찾을 수 없습니다");
+		}
+	}
+
 	protected String addMemo() {
-		Scanner scan = new Scanner(System.in);		
+		Scanner scan = new Scanner(System.in);
 		System.out.print("메모 추가: ");
 		String memo = scan.nextLine();
-		memoList.add(memo);		
+		memoList.add(memo);
 		isModified = true;
 		setNewline();
 		return memo;
 	}
-	
-	protected void writeMemoFile() {
-        try {
-        	PrintWriter printWriter = new PrintWriter(FILENAME);
-        	for (int i=0; i<memoList.size(); i++)
-        		printWriter.println(memoList.get(i));
-            printWriter.close();            
-        } catch (Exception e) {
-        	System.out.println("파일을 찾을 수 없습니다");
-        }
-	}
-	
+
 	private void listMemo() {
 		if (checkEmptyList())
 			return;
-    	for (int i=0; i<memoList.size(); i++)
-    		System.out.println("["+(i+1)+"] "+memoList.get(i));
+		for (int i = 0; i < memoList.size(); i++)
+			System.out.println("[" + (i + 1) + "] " + memoList.get(i));
 		setNewline();
 	}
-	
-	private boolean checkEmptyList() {
-		if (memoList.size()<1) {
-			System.out.println("메모장이 비어 있습니다");
-			return true;
-		}		
-		return false;
-	}
-	
+
 	private void updateMemo() {
 		if (checkEmptyList())
 			return;
 		Scanner scan = new Scanner(System.in);
 		System.out.print("수정할 메모의 번호: ");
-		int id = scan.nextInt()-1;
-		scan.nextLine();
+		int id = scan.nextInt() - 1;
+		scan.nextLine(); //버퍼 비움
 		System.out.print("수정할 내용: ");
 		String content = scan.nextLine();
 		memoList.set(id, content);
 		isModified = true;
 		setNewline();
 	}
-	
+
 	private void deleteMemo() {
 		if (checkEmptyList())
 			return;
-		Scanner scan = new Scanner(System.in);				
+		Scanner scan = new Scanner(System.in);
 		System.out.print("삭제할 메모의 번호: ");
-		int id = scan.nextInt()-1;
+		int id = scan.nextInt() - 1;
 		memoList.remove(id);
 		isModified = true;
 		setNewline();
+	}
+
+	private boolean checkEmptyList() {
+		if (memoList.size() < 1) {
+			System.out.println("메모장이 비어 있습니다");
+			return true;
+		}
+		return false;
 	}
 
 	protected void printMenu() {
@@ -129,7 +130,7 @@ public class MemoManager extends Menu {
 		System.out.println("4. 메모 삭제");
 		System.out.println("5. 뒤로가기");
 	}
-	
+
 	private void setNewline() {
 		System.out.println("");
 	}
