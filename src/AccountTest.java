a4a8b89507f1549e2ec0d0d1167bdc2ffaf9d6fd
Hyemin Kim <1117hyemin@gmail.com>
2017-12-23 22:26:53 +0900

    인코딩 깨짐 문제 수정

--- src/AccountTest.java
+++ src/AccountTest.java
@@ -1,53 +1,54 @@
 import static org.junit.Assert.*;
+
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class AccountTest {
-	AccountManager manager;
-	Record baseRecord, secondRecord;
-	@Before
-	public void setup() {
-		manager = new AccountManager();
-		manager.setup();
+    AccountManager manager;
+    Record baseRecord, secondRecord;
 
-		baseRecord = new Record("2015.12.25", "지지고 1단계", "3500");
-		secondRecord = new Record("2016.12.25", "지지고 2단계", "4000");
-	}
+    @Before
+    public void setup() {
+        manager = new AccountManager();
+        manager.setup();
 
-	@Test
-	public void testCreateRecord() {
-		Record receivedRecord = manager.insertAt(AccountManager.ENDOFLIST, baseRecord);
-		assertArrayEquals(baseRecord.data, receivedRecord.data);
-	}
+        baseRecord = new Record("2015.12.25", "book", "25000");
+        secondRecord = new Record("2016.12.25", "GG-GO", "3500");
+    }
 
-	@Test
-	public void testUpdateRecord() {
-		manager.insertAt(AccountManager.ENDOFLIST,baseRecord);
-		Record result = manager.updateAt(AccountManager.ENDOFLIST, secondRecord);
-		
-		assertArrayEquals(secondRecord.data, result.data);
-	}
+    @Test
+    public void testCreateRecord() {
+        Record receivedRecord = manager.insertAt(AccountManager.ENDOFLIST, baseRecord);
+        assertArrayEquals(baseRecord.data, receivedRecord.data);
+    }
 
-	
-	@Test
-	public void testDeleteRecord() {
-		manager.insertAt(AccountManager.ENDOFLIST,baseRecord);
-		manager.insertAt(AccountManager.ENDOFLIST,secondRecord);
-		manager.deleteAt(AccountManager.ENDOFLIST);
-		
-		Record result = manager.getRecord(AccountManager.ENDOFLIST);
-		
-		assertArrayEquals(baseRecord.data, result.data);
-	}
+    @Test
+    public void testUpdateRecord() {
+        manager.insertAt(AccountManager.ENDOFLIST, baseRecord);
+        Record result = manager.updateAt(AccountManager.ENDOFLIST, secondRecord);
 
-	@After
-	public void tearDown() {
-		try {
-			manager.deleteAt(AccountManager.ENDOFLIST);
-			manager.tearDown();
-		} catch (Exception ex) {
-		}
-	}
+        assertArrayEquals(secondRecord.data, result.data);
+    }
 
+
+    @Test
+    public void testDeleteRecord() {
+        manager.insertAt(AccountManager.ENDOFLIST, baseRecord);
+        manager.insertAt(AccountManager.ENDOFLIST, secondRecord);
+        manager.deleteAt(AccountManager.ENDOFLIST);
+
+        Record result = manager.getRecord(AccountManager.ENDOFLIST);
+
+        assertArrayEquals(baseRecord.data, result.data);
+    }
+
+    @After
+    public void tearDown() {
+        try {
+            manager.deleteAt(AccountManager.ENDOFLIST);
+            manager.tearDown();
+        } catch (Exception ex) {
+        }
+    }
 }
